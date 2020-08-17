abf50144635ffd7fa403a342a226d019b13f85ec
elijahmandeville <elijahmandeville@gmail.com>
2020-08-16 19:09:06 -0600

    mvp & stretch

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,25 @@
+
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    table = dict()
+
+    for i in range(length):
+        current = weights[i]
+        total = (limit - current)
+
+        if total in table:
+            index = table[total]
+
+            return (i, index)
+        else:
+            table[current] = i
 
     return None
+
+
+answer_2 = get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21)
+
+print(answer_2)
